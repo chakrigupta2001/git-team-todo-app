5e19d7e84cb78a5a700b8bfa3c1f918aeaec9802
Narasimha Rao <r.narasimha987@gmail.com>
2026-08-06 23:39:31 +0530

    added java script functionality for to do app

---
+++ script.js
@@ -0,0 +1,20 @@
+const addButton = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addButton.addEventListener("click", function () {
+
+    const task = taskInput.value.trim();
+
+    if (task === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
