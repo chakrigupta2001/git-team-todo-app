c19f2b860d295f78903134e1ec74ea1ed1c49fb0
Chakravarthy Pulikonda <Chakravarthy.P@sunpharma.com>
2026-08-07 16:17:15 +0530

    Adding Log to Jscript

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addButton = document.getElementById("addBtn");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
+console.log("helo");
 
 addButton.addEventListener("click", function () {
 
